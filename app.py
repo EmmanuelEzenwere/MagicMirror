2f7ce679edb86d27ed09af9f6626ba1213a8d680
Nuel Ezenwere <emmaezenwere@gmail.com>
2017-06-09 23:01:26 +0100

    Create app.py

--- app.py
+++ app.py
@@ -14,21 +14,21 @@ __author__ = 'nuelsian'
 # Blueprint("main page", __name__, template_folder="templates")
 
 
-# def query():
-#     """
-#     dummy malicha api query
+def query():
+    """
+    dummy malicha api query
 
-#     :return: json string containing output for processed request.
-#     """
-#     image_input = cv2.imread("w1.jpg")
-#     shape = str(image_input.shape)
-#     image_encoding = str(Enigma.encodeInput(image_input))
-#     hairstyle_name = "weavon_black_3"
-#     category = "tryModel"
-#     user_input = {"image_encoding": image_encoding, "shape": shape, "hairstyle_name": hairstyle_name,
-#                   "category": category}
+    :return: json string containing output for processed request.
+    """
+    image_input = cv2.imread("w1.jpg")
+    shape = str(image_input.shape)
+    image_encoding = str(Enigma.encodeInput(image_input))
+    hairstyle_name = "weavon_black_3"
+    category = "tryModel"
+    user_input = {"image_encoding": image_encoding, "shape": shape, "hairstyle_name": hairstyle_name,
+                  "category": category}
 
-#     return json.dumps(user_input)
+    return json.dumps(user_input)
 
 
 app = Flask(__name__)
